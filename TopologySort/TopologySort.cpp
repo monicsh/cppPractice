2d427a914ceefa10643a33dd909c7744fd4a29ed
Monika Sharma <monic.sh@gmail.com>
2019-10-16 16:07:53 -0700

    modified

--- TopologySort/TopologySort.cpp
+++ TopologySort/TopologySort.cpp
@@ -51,7 +51,8 @@ class TopologicalSort {
       }
 
       // Processing Queue
-      while(!processingQ.empty())
+      int i=0;
+      while(!processingQ.empty() && i < vertices)
       {
           int curr = processingQ.front();
           processingQ.pop();
@@ -67,8 +68,13 @@ class TopologicalSort {
                   indegree.erase(child);
               }
           }
+
+          i++;
       }
 
+      if (!processingQ.empty()){
+          return {};
+      }
       return sortedOrder;
   }
 };
@@ -82,23 +88,62 @@ TEST_CASE("TopologicalSort test cases 001", "[sort]"){
     CHECK(((actual == expected) || (actual == expected1)));
 }
 
-//TEST_CASE("TopologicalSorttest cases 002", "[sort]"){
-//    vector<int> actual =
-//    TopologicalSort::sort(5, vector<vector<int>>{vector<int>{4, 2}, vector<int>{4, 3}, vector<int>{2, 0},vector<int>{2, 1}, vector<int>{3, 1}});
-//
-//    vector<int> expected = {};
-//    REQUIRE(actual == expected);
-//}
-//
-//TEST_CASE("TopologicalSort test cases 003", "[sort]"){
-//    vector<int> actual =
-//    TopologicalSort::sort(7, vector<vector<int>>{vector<int>{6, 4}, vector<int>{6, 2}, vector<int>{5, 3}, vector<int>{5, 4}, vector<int>{3, 0}, vector<int>{3, 1}, vector<int>{3, 2}, vector<int>{4, 1}});
-//
-//    vector<int> expected = {};
-//    REQUIRE(actual == expected);
-//}
+TEST_CASE("TopologicalSorttest cases 002", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(5, vector<vector<int>>{vector<int>{4, 2}, vector<int>{4, 3}, vector<int>{2, 0},vector<int>{2, 1}, vector<int>{3, 1}});
 
+    vector<int> expected1 = {4, 2, 3, 0, 1};
+    vector<int> expected2 = {4, 3, 2, 0, 1};
+    vector<int> expected3 = {4, 3, 2, 1, 0};
+    vector<int> expected4 = {4, 2, 3, 1, 0};
+    vector<int> expected5 = {4, 2, 0, 3, 1};
 
+    CHECK(((actual == expected1) || (actual == expected2) || (actual == expected3) || (actual == expected4) || (actual == expected5)));
+}
 
+TEST_CASE("TopologicalSort test cases 003", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(7, vector<vector<int>>{vector<int>{6, 4}, vector<int>{6, 2}, vector<int>{5, 3}, vector<int>{5, 4}, vector<int>{3, 0}, vector<int>{3, 1}, vector<int>{3, 2}, vector<int>{4, 1}});
 
+    vector<int> expected1 = {5, 6, 3, 4, 0, 1, 2};
+    vector<int> expected2 = {6, 5, 3, 4, 0, 1, 2};
+    vector<int> expected3 = {5, 6, 4, 3, 0, 2, 1};
+    vector<int> expected4 = {6, 5, 4, 3, 0, 1, 2};
+    vector<int> expected5 = {5, 6, 3, 4, 0, 2, 1};
+    vector<int> expected6 = {5, 6, 3, 4, 1, 2, 0};
+
+    CHECK(((actual == expected1) || (actual == expected2) || (actual == expected3) || (actual == expected4) || (actual == expected5) || (actual == expected6) ));
+}
+
+TEST_CASE("TopologicalSort test cases 004 Negative", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(4, vector<vector<int>>{vector<int>{3, 2}, vector<int>{3, 0}, vector<int>{2, 0}, vector<int>{2, 1}});
+
+    vector<int> expected = {};
+    CHECK(((actual != expected)));
+}
+
+TEST_CASE("TopologicalSort test cases 005 Negative Cyclic", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(3, vector<vector<int>>{vector<int>{0, 1}, vector<int>{1, 2}, vector<int>{2, 0}});
+
+    vector<int> expected = {};
+    CHECK(((actual == expected)));
+}
+
+TEST_CASE("TopologicalSort test cases 006", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(3, vector<vector<int>>{vector<int>{0, 1}, vector<int>{1, 2}});
+
+    vector<int> expected = {0, 1, 2};
+    CHECK(((actual == expected)));
+}
+
+TEST_CASE("TopologicalSort test cases 007", "[sort]"){
+    vector<int> actual =
+    TopologicalSort::sort(6, vector<vector<int>>{vector<int>{2, 5}, vector<int>{0, 5}, vector<int>{0, 4}, vector<int>{1, 4}, vector<int>{3, 2}, vector<int>{1, 3}});
+
+    vector<int> expected = {0, 1, 4, 3, 2, 5};
+    CHECK(((actual == expected)));
+}
 
