6e6c83fe88f372276f835a8fe02c3bbecc220c3a
Monika Sharma <monic.sh@gmail.com>
2019-10-17 11:37:52 -0700

    add intervals solutions

---
+++ Intervals/MergeInterval.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+using namespace std;
+
+/*
+ Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+ */
+class MergeIntervals {
+    public:
+        static int maxSumSubarraySizeK(int K, const vector<int>& arr) {
+
+        }
+};
+
+
+TEST_CASE("MaximumSumSubarray test cases 001", "[maxSum]"){
+
+}
+
+
+
